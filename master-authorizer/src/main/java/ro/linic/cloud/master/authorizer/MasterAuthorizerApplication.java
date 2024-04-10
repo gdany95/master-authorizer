7e80dee38a8b617af2b9fc7edd6389ef22d48ec4
Groza Danut <Danut@192.168.11.192>
2024-04-10 12:37:46 +0300

    Initial commit

---
+++ master-authorizer/src/main/java/ro/linic/cloud/master/authorizer/MasterAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package ro.linic.cloud.master.authorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterAuthorizerApplication.class, args);
+	}
+
+}
---
+++ master-authorizer/src/test/java/ro/linic/cloud/master/authorizer/MasterAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.linic.cloud.master.authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
